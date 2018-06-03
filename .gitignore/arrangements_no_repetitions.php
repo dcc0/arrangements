16847b1347336d6f8875b1586ffcf8651b7bc70d
dcc0 <dcc0@yandex.ru>
2018-06-03 17:53:08 +0300

    Update arrangements_no_repetitions.php

--- .gitignore/arrangements_no_repetitions.php
+++ .gitignore/arrangements_no_repetitions.php
@@ -17,7 +17,6 @@ $a = array(
 
 $k = 5; /*Set a value. Ваше значение.*/
 $n = 7; /*Set a value. Ваше значение.*/
-
 /*We cut it for two arrays:
 * in B - elements with element on a position K and
 * in C the tail of elements.
@@ -29,50 +28,48 @@ $b = array_splice($a, 0, $k);
 
 /*Print first. Печтаем первое вне циклов (поэтому почти эвристика)*/
 permute($b,$h);
-
 while (1)
 	{
-
+	
 	/*Here we search an element in array C greater than K in array B.
 	* Ищем элемент в массиве С, больше на единицу, чем
 	* элемент в B на позиции K*/
 	$more_per_unit = array_search($b[$k - 1] + 1, $c);
 	if ($more_per_unit !== false)
 		{
-
+		
 		/*Found. Change. Нашли.
 		* Транспортируем единицу*/
 		$c[$more_per_unit]-= 1;
-		$b[$k - 1] = $b[$k - 1] + 1;
+		$b[$k - 1] += 1;
 		permute($b,$h);
 		}
-
+	
 	/*Если последний элемент в массива B равен n
 	* - старшему значению, то цикл поиска элемента слева от К,
 	* для которого в массиве C есть больший на единицу.*/
 	if ($b[$k - 1] == $n)
 		{
 		$i = $k - 1;
-
+		
 		/*Просмотр массива справа налево.
 		* Looking greater element (from right to left).*/
 		while ($i >= 0)
 			{
-
+			
 			/*Если дошли до 0 индекса и таких элементов нет,
 			* то алгоритм завершается.
 			* Index 0 and we didn't find the element.
 			* This condition stops the algorithm.*/
 			if ($i == 0 && $b[$i] == $n - $k + 1) break 2;
-
 			/*Поиск элемента для увеличения.
 			* Looking for an element to increase it.*/
 			$more_per_unit = array_search($b[$i] + 1, $c);
 			if ($more_per_unit !== false)
 				{
-				$c[$more_per_unit] = $c[$more_per_unit] - 1;
-				$b[$i] = $b[$i] + 1;
-
+				$c[$more_per_unit] -=  1;
+				$b[$i] += 1;
+				
 				/*Перенос значений в С. Заполнение  массива B до K*.
 				*Here we transport elements to C and fill array B till K.*/
 				for ($j = $i; $j != $k - 1; $j++)
@@ -80,7 +77,7 @@ while (1)
 					array_unshift($c, $b[$j + 1]);
 					$b[$j + 1] = $b[$j] + 1;
 					}
-
+				
 				/*Удаление повторяющихся значений из C.*
 				* Here we remove duplicates out of C.*/
 				$c = array_diff($c, $b);
@@ -91,8 +88,6 @@ while (1)
 			}
 		}
 	}
-
-
 /*Функция перестановок. Permutations function.*/
 function permute($b,&$h)
 	{
@@ -105,13 +100,13 @@ function permute($b,&$h)
 	$a = array_reverse($b);
 	while (1)
 		{
-
+		
 		/*Печать и условие выхода. Here we print and exit.*/
 		print_r($a);
 		print "\n";
 		$h++;
 		if ($a == $b) break;
-
+		
 		/*Ищем следующую перестановку.
 		 * Here we search next permutation.*/
 		$i = 1;
@@ -124,11 +119,12 @@ function permute($b,&$h)
 			{
 			$j++;
 			}
+		
 		/*Обмен. Here we change.*/
 		$c = $a[$j];
 		$a[$j] = $a[$i];
 		$a[$i] = $c;
-
+		
 		/*Обернем хвост. Tail reverse.*/
 		$c = $a;
 		$z = 0;
